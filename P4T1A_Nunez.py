15e7a5f9baf1821c469eeeaa08418b3873ca4cb7
nuneza6954 <47428238+nuneza6954@users.noreply.github.com>
2019-02-28 13:47:56 -0500

    Add files via upload
    Turtle Graphics

---
+++ P4T1A_Nunez.py
@@ -0,0 +1,33 @@
+# Ali Nunez
+# 02-28-2019
+# Turtle Lab
+# CTI-110 P4T1A - Shapes
+# This program will use a while loop or for loop that draws both a square
+# and a triangle.
+
+
+
+for i in range(5):
+    import turtle
+    turtle.shape("turtle")
+
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+
+    turtle.forward(80)        
+    turtle.left(120)
+    turtle.forward(80)
+    turtle.left(120)
+    turtle.forward(80)
+    turtle.left(120)   
+
+# Made a for loop
+# Imported my Turtle
+# Changed the shape of the turtle from an arrow to a Turtle
+# Made Turtle draw out a sqaure and a triangle. 
---
+++ P4T1B_Nunez.py
@@ -0,0 +1,40 @@
+# Ali Nunez
+# 02-28-2019
+# Turtle Lab 
+# CTI-110 P4T1B - Initials 
+# This program will displays my first and last initials
+
+import turtle
+turtle.shape("turtle")
+
+turtle.color("blue")
+turtle.pensize(3)
+
+# First Initial
+turtle.left(70)
+turtle.forward(60)
+turtle.right(140)
+turtle.forward(60)
+turtle.left(180)
+turtle.forward(30)
+turtle.left(70)
+turtle.forward(20)
+
+# Second Initial
+turtle.penup()
+turtle.right(180)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(30)
+
+turtle.pendown()
+
+turtle.left(180)
+turtle.forward(50)
+turtle.right(150)
+turtle.forward(58)
+turtle.left(150)
+turtle.forward(50)
+
+
+ 
