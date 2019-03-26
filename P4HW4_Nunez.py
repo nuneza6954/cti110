fccdb102bae70118d7853183aeda9787876eb110
nuneza6954 <47428238+nuneza6954@users.noreply.github.com>
2019-03-26 11:14:16 -0400

    Add files via upload
    Nested Loops

---
+++ P4HW4_Nunez.py
@@ -0,0 +1,94 @@
+# Ali Nunez
+# 03-21-2019
+# Turtle Lab
+# CTI-110 P4HW4: Nested Loops
+# This program will draw a shape using a nested loop. 
+
+
+
+for i in range(5):
+    import turtle
+    turtle.speed(10)
+    turtle.shape("turtle")
+
+    turtle.forward(144)
+
+    for i in range(1):
+        turtle.right(144)
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(80)
+turtle.pendown()
+
+for i in range(8):
+    import turtle
+    turtle.speed(10)
+    turtle.shape("turtle")
+
+    turtle.forward(144)
+
+    for i in range(1):
+        turtle.right(45)
+        turtle.backward(20)
+        turtle.left(45)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.left(45)
+        turtle.backward(20) 
+        turtle.left(45)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.left(45)
+        turtle.backward(20) 
+        turtle.left(45)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.left(45)
+        turtle.backward(20) 
+        turtle.left(45)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.backward(20)
+        turtle.left(45)
+        turtle.backward(20)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
